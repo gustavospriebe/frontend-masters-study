3b5394f6caad5529ef14a0cf3067e818715268e9
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-04-05 00:44:34 -0300

    finish intro to next

---
+++ nextjs/blog-next/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <div>
+                <div>name</div>
+                <div>blog</div>
+                <div>contact</div>
+            </div>
+        </div>
+    );
+}
